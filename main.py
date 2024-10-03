a8681b791db06b74c301499d84061ba53e7c9bac
Apurva Sharma <apurva.89.sharma@gmail.com>
2024-10-03 21:21:41 +0200

    add method

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import random
 class RockScissorspaper:
     def __init__(self):
         self.choices = ['rock', 'scissors', 'paper']
+            def computer_choice(self):
+        return random.choice(self.choices)
