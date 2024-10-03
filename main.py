f10945283c40ea9c787578c11e3c706be789cc56
Apurva Sharma <apurva.89.sharma@gmail.com>
2024-10-03 21:24:38 +0200

    winner determination logic

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ class RockScissorspaper:
         self.choices = ['rock', 'scissors', 'paper']
             def computer_choice(self):
         return random.choice(self.choices)
+            def determine_winner(self, player_choice, computer_choice):
+        if player_choice == computer_choice:
+            return "tie"
+        elif (player_choice == 'rock' and computer_choice == 'scissors') or \
+             (player_choice == 'scissors' and computer_choice == 'paper') or \
+             (player_choice == 'paper' and computer_choice == 'rock'):
+            return "player"
+        else:
+            return "computer"
+
